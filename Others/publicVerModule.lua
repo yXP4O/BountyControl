5caa56cb4c1b5410e2fea89c7dd2526104c0f5eb
yXP4O <125714821+yXP4O@users.noreply.github.com>
2023-02-19 11:57:42 -1000

    Update publicVerModule.lua

--- Others/publicVerModule.lua
+++ Others/publicVerModule.lua
@@ -10,4 +10,26 @@ functions.findTool = function(baseName)
     return nil
 end
 
+functions.fetchJob = function()
+    local url = "https://games.roblox.com/v1/games/2788229376/servers/Public?sortOrder=Asc&limit=100"
+    local http = game:service"HttpService"
+    local req = game:HttpGet(url)
+    
+    local job = ""
+    local lowest = 41
+    local lowestPing = 100000
+    for i,v in pairs(http:JSONDecode(req)) do
+        if type(v) == "table" then
+            for c,n in pairs(v) do
+                if lowest > n["playing"] and lowestPing > n["ping"] then
+                    lowest = n["playing"]
+                    job = n["id"]
+                    lowestPing = n["ping"]
+                end
+            end
+        end
+    end
+    return job
+end
+
 return functions
